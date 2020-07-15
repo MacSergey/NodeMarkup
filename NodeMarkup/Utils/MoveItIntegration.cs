16e8ab9f233ea23fabe1cd5a9a72966ddb0aa729
MacSergey <macsergey@me.com>
2020-07-15 21:50:46 +0300

    MoveIt Integration

---
+++ NodeMarkup/Utils/MoveItIntegration.cs
@@ -0,0 +1,50 @@
+﻿using MoveItIntegration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NodeMarkup.Manager;
+
+namespace NodeMarkup.Utils
+{
+    public class MoveItIntegrationFactory : IMoveItIntegrationFactory
+    {
+        public string Name => throw new NotImplementedException();
+
+        public string Description => throw new NotImplementedException();
+
+        public IMoveItIntegration GetInstance() => new MoveItIntegration();
+    }
+
+    public class MoveItIntegration : IMoveItIntegration
+    {
+        public object CopyNode(ushort nodeID)
+        {
+            if (MarkupManager.TryGetMarkup(nodeID, out Markup markup))
+            {
+                var data = markup.ToXml();
+                return data;
+            }
+            else
+                return null;
+        }
+
+        public object CopySegment(ushort segmentId) => null;
+        public object Decode64(string base64Data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Encode64(object record)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PasteNode(ushort nodeID, object record)
+        {
+            
+        }
+
+        public void PasteSegment(ushort segmentId, object record, Dictionary<InstanceID, InstanceID> map) { }
+    }
+}
