79f017941f7ddad4835174156bf5513b4df89392
MacSergey <macsergey@me.com>
2020-07-16 00:19:35 +0300

    Beta localize

--- NodeMarkup/Properties/AssemblyInfo.cs
+++ NodeMarkup/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 #if DEBUG
-[assembly: AssemblyVersion("1.2.0.19")]
-[assembly: AssemblyFileVersion("1.2.0.19")]
+[assembly: AssemblyVersion("1.2.0.23")]
+[assembly: AssemblyFileVersion("1.2.0.23")]
 #else
 [assembly: AssemblyVersion("1.1")]
 [assembly: AssemblyFileVersion("1.1")]
