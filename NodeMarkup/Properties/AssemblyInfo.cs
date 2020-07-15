1f913c806805bae5439b58f6a36a4a0e289e3291
MacSergey <macsergey@me.com>
2020-07-16 00:06:07 +0300

    Copy with MoveIt

--- NodeMarkup/Properties/AssemblyInfo.cs
+++ NodeMarkup/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 #if DEBUG
-[assembly: AssemblyVersion("1.2.0.14")]
-[assembly: AssemblyFileVersion("1.2.0.14")]
+[assembly: AssemblyVersion("1.2.0.19")]
+[assembly: AssemblyFileVersion("1.2.0.19")]
 #else
 [assembly: AssemblyVersion("1.1")]
 [assembly: AssemblyFileVersion("1.1")]
