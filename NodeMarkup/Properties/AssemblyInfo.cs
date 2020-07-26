6cbce84700dfe076cd82d3e775443ef30a53e80c
MacSergey <macsergey@me.com>
2020-07-26 22:03:26 +0300

    output files

--- NodeMarkup/Properties/AssemblyInfo.cs
+++ NodeMarkup/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 #if DEBUG
-[assembly: AssemblyVersion("1.2.0.476")]
-[assembly: AssemblyFileVersion("1.2.0.468")]
+[assembly: AssemblyVersion("1.2.0.486")]
+[assembly: AssemblyFileVersion("1.2.0.478")]
 #else
 [assembly: AssemblyVersion("1.1")]
 [assembly: AssemblyFileVersion("1.1")]
