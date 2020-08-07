845a9101b8a52b852b6ed0777e3b887b6cc81e96
MacSergey <macsergey@me.com>
2020-08-08 01:43:53 +0300

    Unlock functions in beta

--- NodeMarkup/EarlyAccess.cs
+++ NodeMarkup/EarlyAccess.cs
@@ -49,11 +49,17 @@ namespace NodeMarkup
 
         public static bool CheckFunctionAccess(string function, bool alert = true)
         {
+#if !DEBUG
             if (Status)
                 return true;
-            else if (alert)
+#endif
+            if (alert)
                 ShowNoEarlyAccess(Localize.EarlyAccess_FunctionUnavailableCaption, string.Format(Localize.EarlyAccess_FunctionUnavailableMessage, function));
+#if !DEBUG
             return false;
+#else
+            return true;
+#endif
         }
         public static void ShowNoEarlyAccess(string caption, string message)
         {
