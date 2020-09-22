fc46c0f965a9433cd1dd2de34cd7a59e49d5cdb8
MacSergey <macsergey@me.com>
2020-09-22 20:06:23 +0300

    new line style icons

--- NodeMarkup/Properties/AssemblyInfo.cs
+++ NodeMarkup/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 #if DEBUG
-[assembly: AssemblyVersion("1.4.0.9")]
-[assembly: AssemblyFileVersion("1.4.0.9")]
+[assembly: AssemblyVersion("1.4.0.11")]
+[assembly: AssemblyFileVersion("1.4.0.11")]
 #else
 [assembly: AssemblyVersion("1.3")]
 [assembly: AssemblyFileVersion("1.3")]
