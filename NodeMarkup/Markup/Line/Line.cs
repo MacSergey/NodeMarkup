96348566b655cf157e14033b6bb6f78e42f3bf91
MacSergey <macsergey@me.com>
2020-11-12 23:06:11 +0300

    update line

--- NodeMarkup/Markup/Line/Line.cs
+++ NodeMarkup/Markup/Line/Line.cs
@@ -283,11 +283,11 @@ namespace NodeMarkup.Manager
             if (!Settings.CutLineByCrosswalk || point.Type == MarkupPoint.PointType.Normal)
                 return new EnterPointEdge(point);
 
-            var intersects = Markup.GetIntersects(this).Where(i => i.Pair.GetOther(this) is MarkupCrosswalkLine line && line.PointPair.ContainsEnter(point.Enter)).ToArray();
-            if (!intersects.Any(i => i.IsIntersect))
+            var intersects = Markup.GetIntersects(this).Where(i => i.IsIntersect && i.Pair.GetOther(this) is MarkupCrosswalkLine line && line.PointPair.ContainsEnter(point.Enter)).ToArray();
+            if (!intersects.Any())
                 return new EnterPointEdge(point);
 
-            var intersect = intersects.Where(i => i.IsIntersect).Aggregate((i, j) => point == End ^ (i.FirstT > i.SecondT) ? i : j);
+            var intersect = intersects.Aggregate((i, j) => point == End ^ (i.FirstT > i.SecondT) ? i : j);
             return new LinesIntersectEdge(intersect.Pair);
         }
 
