8112a8d6ae10338cdd99c06c85185c2f682c1724
MacSergey <macsergey@me.com>
2021-03-12 22:50:35 +0300

    update version

--- NodeMarkup/Properties/AssemblyInfo.cs
+++ NodeMarkup/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
 #if BETA
-[assembly: AssemblyVersion("1.6.0.501")]
-[assembly: AssemblyFileVersion("1.6.0.501")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
 #else
 [assembly: AssemblyVersion("1.6")]
 [assembly: AssemblyFileVersion("1.6")] 
