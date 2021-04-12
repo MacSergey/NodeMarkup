c12ce20dd41da924f210e53f433137bd833b03dc
MacSergey <macsergey@me.com>
2021-04-12 11:03:22 +0300

    fix filler edge enter lines auto creation

--- NodeMarkup/Markup/Filler/FillerVertex.cs
+++ NodeMarkup/Markup/Filler/FillerVertex.cs
@@ -88,6 +88,7 @@ namespace NodeMarkup.Manager
         {
             EnterFillerVertexBase otherE when Enter == otherE.Enter => new MarkupEnterLine(Point.Markup, Point, otherE.Point, Alignment, otherE.Alignment),
             EnterFillerVertexBase otherE when Point.Lines.Intersect(otherE.Point.Lines).FirstOrDefault() is MarkupLine line => line,
+            EnterFillerVertexBase otherE when (Enter.Next == otherE.Enter && Point.IsLast && otherE.Point.IsFirst) || (Enter.Prev == otherE.Enter && Point.IsFirst && otherE.Point.IsLast) => new MarkupEnterLine(Point.Markup, Point, otherE.Point, Alignment, otherE.Alignment),
             EnterFillerVertexBase otherE => new MarkupRegularLine(Point.Markup, Point, otherE.Point, alignment: Point.IsSplit ? Alignment : (otherE.Point.IsSplit ? otherE.Alignment.Invert() : Alignment.Centre)),
             IntersectFillerVertex otherI => otherI.LinePair.First.ContainsPoint(Point) ? otherI.LinePair.First : otherI.LinePair.Second,
             _ => null,
@@ -131,7 +132,7 @@ namespace NodeMarkup.Manager
                     }
                 }
             }
-            
+
             if (contour.First is EnterFillerVertexBase first && first.Enter == Enter && (first.Point.Num == minNum || first.Point.Num == maxNum))
                 yield return first;
         }
