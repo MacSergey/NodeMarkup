d0286c65a074f1b1acc5091cf1faf3000675fc6b
MacSergey <macsergey@me.com>
2021-05-07 18:34:02 +0300

    fix create edge lines

--- NodeMarkup/Properties/AssemblyInfo.cs
+++ NodeMarkup/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Runtime.InteropServices;
 [assembly: Guid("94b9b12b-0d2b-4685-bf64-4f0488470569")]
 
 #if BETA
-[assembly: AssemblyVersion("1.8.0.245")]
-[assembly: AssemblyFileVersion("1.8.0.245")]
+[assembly: AssemblyVersion("1.8.0.246")]
+[assembly: AssemblyFileVersion("1.8.0.246")]
 #else
 [assembly: AssemblyVersion("1.7.1")]
 [assembly: AssemblyFileVersion("1.7.1")] 
