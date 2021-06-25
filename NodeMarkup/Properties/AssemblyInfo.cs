ecc5d1251b041e588d46e65b4be6a16384a105d4
MacSergey <macsergey@me.com>
2021-06-25 11:25:46 +0300

    Added indonesian translation

--- NodeMarkup/Properties/AssemblyInfo.cs
+++ NodeMarkup/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Runtime.InteropServices;
 [assembly: Guid("94b9b12b-0d2b-4685-bf64-4f0488470569")]
 
 #if BETA
-[assembly: AssemblyVersion("1.8.0.509")]
-[assembly: AssemblyFileVersion("1.8.0.509")]
+[assembly: AssemblyVersion("1.8.0.510")]
+[assembly: AssemblyFileVersion("1.8.0.510")]
 #else
 [assembly: AssemblyVersion("1.7.3.3")]
 [assembly: AssemblyFileVersion("1.7.3.3")] 
