a49af0c2edafe6f8437117ca5d123e3fdcb41b2b
MacSergey <macsergey@me.com>
2021-08-25 14:04:18 +0300

    Update version

--- NodeMarkup/Properties/AssemblyInfo.cs
+++ NodeMarkup/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Runtime.InteropServices;
 [assembly: Guid("94b9b12b-0d2b-4685-bf64-4f0488470569")]
 
 #if BETA
-[assembly: AssemblyVersion("1.8.0.698")]
-[assembly: AssemblyFileVersion("1.8.0.698")]
+[assembly: AssemblyVersion("1.8.0.699")]
+[assembly: AssemblyFileVersion("1.8.0.699")]
 #else
 [assembly: AssemblyVersion("1.8.2")]
 [assembly: AssemblyFileVersion("1.8.2")] 
