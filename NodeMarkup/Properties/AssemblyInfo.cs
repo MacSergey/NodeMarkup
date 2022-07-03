924224f7c17aec55c48ebde3ba5eb711d25cef74
MacSergey <macsergey@me.com>
2022-07-03 09:34:35 +0300

    Fixed 3D fillers illumination

--- NodeMarkup/Properties/AssemblyInfo.cs
+++ NodeMarkup/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@ using System.Runtime.InteropServices;
 [assembly: Guid("94b9b12b-0d2b-4685-bf64-4f0488470569")]
 
 #if BETA
-[assembly: AssemblyVersion("1.10.0.248")]
-[assembly: AssemblyFileVersion("1.10.0.248")]
+[assembly: AssemblyVersion("1.10.0.254")]
+[assembly: AssemblyFileVersion("1.10.0.254")]
 #else
-[assembly: AssemblyVersion("1.10")]
-[assembly: AssemblyFileVersion("1.10")] 
+[assembly: AssemblyVersion("1.10.0.1")]
+[assembly: AssemblyFileVersion("1.10.0.1")] 
 #endif
