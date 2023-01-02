7995244c55bdaed5f853f35fb90a446078fc2f68
MacSergey <macsergey@me.com>
2023-01-02 03:24:14 +0300

    save and load markings for different types of elevation

--- NodeMarkup/Manager/Extensions/AssetDataExtension.cs
+++ NodeMarkup/Manager/Extensions/AssetDataExtension.cs
@@ -1,15 +1,8 @@
-﻿using ColossalFramework;
-using ColossalFramework.UI;
-using HarmonyLib;
-using ICities;
-using ModsCommon;
+﻿using ModsCommon;
 using ModsCommon.Utilities;
 using NodeMarkup.Manager;
 using NodeMarkup.Utilities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using System.Xml.Linq;
 
 namespace NodeMarkup
@@ -27,7 +20,15 @@ namespace NodeMarkup
     public class NetworkAssetDataExtension : BaseNetworkAssetDataExtension<Mod, NetworkAssetDataExtension, ObjectsMap>
     {
         protected override string DataId { get; } = $"{Loader.Id}.Data";
+        protected string DataElevatedId { get; } = $"{Loader.Id}.Data_Elevated";
+        protected string DataBridgeId { get; } = $"{Loader.Id}.Data_Bridge";
+        protected string DataSlopeId { get; } = $"{Loader.Id}.Data_Slope";
+        protected string DataTunnelId { get; } = $"{Loader.Id}.Data_Tunnel";
         protected override string MapId { get; } = $"{Loader.Id}.Map";
+        protected string MapElevatedId { get; } = $"{Loader.Id}.Map_Elevated";
+        protected string MapBridgeId { get; } = $"{Loader.Id}.Map_Bridge";
+        protected string MapSlopeId { get; } = $"{Loader.Id}.Map_Slope";
+        protected string MapTunnelId { get; } = $"{Loader.Id}.Map_Tunnel";
 
         protected override ObjectsMap CreateMap(bool isSimple) => new ObjectsMap(isSimple: isSimple);
         protected override XElement GetConfig(NetInfo prefab, out ushort segmentId, out ushort startNodeId, out ushort endNodeId)
@@ -61,13 +62,54 @@ namespace NodeMarkup
                 if (prefab.m_netAI != null)
                 {
                     if (GetNetInfo(prefab.m_netAI, nameof(RoadAI.m_elevatedInfo), out var elevatedInfo))
-                        AssetDatas[elevatedInfo] = data;
+                    {
+                        if (Load(prefab, userData, out var elevatedData, DataElevatedId, MapElevatedId))
+                            AssetDatas[elevatedInfo] = elevatedData;
+                        else
+                            AssetDatas[elevatedInfo] = data;
+                    }
                     if (GetNetInfo(prefab.m_netAI, nameof(RoadAI.m_bridgeInfo), out var bridgeInfo))
-                        AssetDatas[bridgeInfo] = data;
+                    {
+                        if (Load(prefab, userData, out var bridgeData, DataBridgeId, MapBridgeId))
+                            AssetDatas[bridgeInfo] = bridgeData;
+                        else
+                            AssetDatas[bridgeInfo] = data;
+                    }
                     if (GetNetInfo(prefab.m_netAI, nameof(RoadAI.m_slopeInfo), out var slopeInfo))
-                        AssetDatas[slopeInfo] = data;
+                    {
+                        if (Load(prefab, userData, out var slopeData, DataSlopeId, MapSlopeId))
+                            AssetDatas[slopeInfo] = slopeData;
+                        else
+                            AssetDatas[slopeInfo] = data;
+                    }
                     if (GetNetInfo(prefab.m_netAI, nameof(RoadAI.m_tunnelInfo), out var tunnelInfo))
-                        AssetDatas[tunnelInfo] = data;
+                    {
+                        if (Load(prefab, userData, out var tunnelData, DataTunnelId, MapTunnelId))
+                            AssetDatas[tunnelInfo] = tunnelData;
+                        else
+                            AssetDatas[tunnelInfo] = data;
+                    }
+                }
+            }
+        }
+        public override void OnAssetSaved(string name, object asset, out Dictionary<string, byte[]> userData)
+        {
+            userData = new Dictionary<string, byte[]>();
+
+            if (asset is NetInfo prefab)
+            {
+                Save(prefab, userData);
+
+                if (prefab.m_netAI != null)
+                {
+                    if (GetNetInfo(prefab.m_netAI, nameof(RoadAI.m_elevatedInfo), out var elevatedInfo))
+                        Save(elevatedInfo, userData, DataElevatedId, MapElevatedId);
+                    if (GetNetInfo(prefab.m_netAI, nameof(RoadAI.m_bridgeInfo), out var bridgeInfo))
+                        Save(bridgeInfo, userData, DataBridgeId, MapBridgeId);
+                    if (GetNetInfo(prefab.m_netAI, nameof(RoadAI.m_slopeInfo), out var slopeInfo))
+                        Save(slopeInfo, userData, DataSlopeId, MapSlopeId);
+                    if (GetNetInfo(prefab.m_netAI, nameof(RoadAI.m_tunnelInfo), out var tunnelInfo))
+                        Save(tunnelInfo, userData, DataTunnelId, MapTunnelId);
                 }
             }
         }
