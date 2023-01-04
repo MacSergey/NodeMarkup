0ee7eba8e4444d6716b92e192c43440450ca67ed
MacSergey <macsergey@me.com>
2023-01-04 17:36:05 +0300

    catch render exeptions

--- NodeMarkup/Utilities/Render/ObjectRender.cs
+++ NodeMarkup/Utilities/Render/ObjectRender.cs
@@ -53,9 +53,6 @@ namespace NodeMarkup.Utilities
 
             foreach (var item in Items)
                 RenderInstance(cameraInfo, Info, instance, item.Position, item.Scale, item.Angle, item.Tilt, item.Slope, item.Color, new Vector4(), true);
-
-            //foreach (var item in Items)
-            //    PropInstance.RenderInstance(cameraInfo, Info, instance, item.Position, item.Scale, item.Angle, item.Color, new Vector4(), true);
         }
 
         public static void RenderInstance(RenderManager.CameraInfo cameraInfo, PropInfo info, InstanceID id, Vector3 position, float scale, float angle, float tilt, float slope, Color color, Vector4 objectIndex, bool active)
@@ -65,14 +62,14 @@ namespace NodeMarkup.Utilities
 
             if (info.m_hasEffects && (active || info.m_alwaysActive))
             {
-                Matrix4x4 matrix = default(Matrix4x4);
-                matrix.SetTRS(position, Quaternion.AngleAxis(angle * 57.29578f, Vector3.down), new Vector3(scale, scale, scale));
-                float simulationTimeDelta = Singleton<SimulationManager>.instance.m_simulationTimeDelta;
+                var matrix = default(Matrix4x4);
+                matrix.SetTRS(position, Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.down), new Vector3(scale, scale, scale));
+                var simulationTimeDelta = Singleton<SimulationManager>.instance.m_simulationTimeDelta;
                 for (int i = 0; i < info.m_effects.Length; i++)
                 {
-                    Vector3 position2 = matrix.MultiplyPoint(info.m_effects[i].m_position);
-                    Vector3 direction = matrix.MultiplyVector(info.m_effects[i].m_direction);
-                    EffectInfo.SpawnArea area = new EffectInfo.SpawnArea(position2, direction, 0f);
+                    var effectPos = matrix.MultiplyPoint(info.m_effects[i].m_position);
+                    var effectDir = matrix.MultiplyVector(info.m_effects[i].m_direction);
+                    EffectInfo.SpawnArea area = new EffectInfo.SpawnArea(effectPos, effectDir, 0f);
                     info.m_effects[i].m_effect.RenderEffect(id, area, Vector3.zero, 0f, 1f, -1f, simulationTimeDelta, cameraInfo);
                 }
             }
@@ -85,18 +82,18 @@ namespace NodeMarkup.Utilities
                     objectIndex.z = 0f;
                 else if (info.m_illuminationOffRange.x < 1000f || info.m_illuminationBlinkType != 0)
                 {
-                    LightSystem lightSystem = Singleton<RenderManager>.instance.lightSystem;
-                    Randomizer randomizer = new Randomizer(id.Index);
-                    var num = info.m_illuminationOffRange.x + (float)randomizer.Int32(100000u) * 1E-05f * (info.m_illuminationOffRange.y - info.m_illuminationOffRange.x);
-                    objectIndex.z = MathUtils.SmoothStep(num + 0.01f, num - 0.01f, lightSystem.DayLightIntensity);
+                    var lightSystem = Singleton<RenderManager>.instance.lightSystem;
+                    var randomizer = new Randomizer(id.Index);
+                    var illumination = info.m_illuminationOffRange.x + randomizer.Int32(100000u) * 1E-05f * (info.m_illuminationOffRange.y - info.m_illuminationOffRange.x);
+                    objectIndex.z = MathUtils.SmoothStep(illumination + 0.01f, illumination - 0.01f, lightSystem.DayLightIntensity);
                     if (info.m_illuminationBlinkType != 0)
                     {
-                        Vector4 blinkVector = LightEffect.GetBlinkVector(info.m_illuminationBlinkType);
-                        float num2 = num * 3.71f + Singleton<SimulationManager>.instance.m_simulationTimer / blinkVector.w;
-                        num2 = (num2 - Mathf.Floor(num2)) * blinkVector.w;
-                        float num3 = MathUtils.SmoothStep(blinkVector.x, blinkVector.y, num2);
-                        float num4 = MathUtils.SmoothStep(blinkVector.w, blinkVector.z, num2);
-                        objectIndex.z *= 1f - num3 * num4;
+                        var blinkVector = LightEffect.GetBlinkVector(info.m_illuminationBlinkType);
+                        illumination = illumination * 3.71f + Singleton<SimulationManager>.instance.m_simulationTimer / blinkVector.w;
+                        illumination = (illumination - Mathf.Floor(illumination)) * blinkVector.w;
+                        var illuminationX = MathUtils.SmoothStep(blinkVector.x, blinkVector.y, illumination);
+                        var illuminationY = MathUtils.SmoothStep(blinkVector.w, blinkVector.z, illumination);
+                        objectIndex.z *= 1f - illuminationX * illuminationY;
                     }
                 }
                 else
@@ -105,10 +102,10 @@ namespace NodeMarkup.Utilities
 
             if (cameraInfo == null || cameraInfo.CheckRenderDistance(position, Settings.PropLODDistance))
             {
-                Matrix4x4 matrix = default(Matrix4x4);
+                var matrix = default(Matrix4x4);
                 matrix.SetTRS(position, Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.down) * Quaternion.AngleAxis(tilt * Mathf.Rad2Deg, Vector3.right) * Quaternion.AngleAxis(slope * Mathf.Rad2Deg, Vector3.forward), new Vector3(scale, scale, scale));
-                PropManager instance = Singleton<PropManager>.instance;
-                MaterialPropertyBlock materialBlock = instance.m_materialBlock;
+                var instance = Singleton<PropManager>.instance;
+                var materialBlock = instance.m_materialBlock;
                 materialBlock.Clear();
                 materialBlock.SetColor(instance.ID_Color, color);
                 materialBlock.SetVector(instance.ID_ObjectIndex, objectIndex);
@@ -122,20 +119,20 @@ namespace NodeMarkup.Utilities
             }
             else if (info.m_lodMaterialCombined == null)
             {
-                Matrix4x4 matrix = default(Matrix4x4);
+                var matrix = default(Matrix4x4);
                 matrix.SetTRS(position, Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.down) * Quaternion.AngleAxis(tilt * Mathf.Rad2Deg, Vector3.right) * Quaternion.AngleAxis(slope * Mathf.Rad2Deg, Vector3.forward), new Vector3(scale, scale, scale));
-                PropManager instance2 = Singleton<PropManager>.instance;
-                MaterialPropertyBlock materialBlock2 = instance2.m_materialBlock;
-                materialBlock2.Clear();
-                materialBlock2.SetColor(instance2.ID_Color, color);
-                materialBlock2.SetVector(instance2.ID_ObjectIndex, objectIndex);
+                var instance = Singleton<PropManager>.instance;
+                var materialBlock = instance.m_materialBlock;
+                materialBlock.Clear();
+                materialBlock.SetColor(instance.ID_Color, color);
+                materialBlock.SetVector(instance.ID_ObjectIndex, objectIndex);
                 if (info.m_rollLocation != null)
                 {
-                    info.m_material.SetVectorArray(instance2.ID_RollLocation, info.m_rollLocation);
-                    info.m_material.SetVectorArray(instance2.ID_RollParams, info.m_rollParams);
+                    info.m_material.SetVectorArray(instance.ID_RollLocation, info.m_rollLocation);
+                    info.m_material.SetVectorArray(instance.ID_RollParams, info.m_rollParams);
                 }
-                instance2.m_drawCallData.m_defaultCalls++;
-                Graphics.DrawMesh(info.m_lodMesh, matrix, info.m_lodMaterial, RenderHelper.RoadLayer, null, 0, materialBlock2);
+                instance.m_drawCallData.m_defaultCalls++;
+                Graphics.DrawMesh(info.m_lodMesh, matrix, info.m_lodMaterial, RenderHelper.RoadLayer, null, 0, materialBlock);
             }
             else
             {
@@ -169,14 +166,14 @@ namespace NodeMarkup.Utilities
 
             if (cameraInfo == null || info.m_lodMesh1 == null || cameraInfo.CheckRenderDistance(position, Settings.TreeLODDistance))
             {
-                TreeManager instance = Singleton<TreeManager>.instance;
-                MaterialPropertyBlock materialBlock = instance.m_materialBlock;
-                Matrix4x4 matrix = default(Matrix4x4);
+                var instance = Singleton<TreeManager>.instance;
+                var materialBlock = instance.m_materialBlock;
+                var matrix = default(Matrix4x4);
                 matrix.SetTRS(position, Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.down) * Quaternion.AngleAxis(tilt * Mathf.Rad2Deg, Vector3.right) * Quaternion.AngleAxis(slope * Mathf.Rad2Deg, Vector3.forward), new Vector3(scale, scale, scale));
-                Color value = info.m_defaultColor * brightness;
-                value.a = Singleton<WeatherManager>.instance.GetWindSpeed(position);
+                var color = info.m_defaultColor * brightness;
+                color.a = Singleton<WeatherManager>.instance.GetWindSpeed(position);
                 materialBlock.Clear();
-                materialBlock.SetColor(instance.ID_Color, value);
+                materialBlock.SetColor(instance.ID_Color, color);
                 materialBlock.SetVector(instance.ID_ObjectIndex, objectIndex);
                 instance.m_drawCallData.m_defaultCalls++;
                 Graphics.DrawMesh(info.m_mesh, matrix, info.m_material, RenderHelper.RoadLayer, null, 0, materialBlock);
@@ -184,7 +181,7 @@ namespace NodeMarkup.Utilities
             else
             {
                 position.y += info.m_generatedInfo.m_center.y * (scale - 1f);
-                Color color = info.m_defaultColor * brightness;
+                var color = info.m_defaultColor * brightness;
                 color.a = Singleton<WeatherManager>.instance.GetWindSpeed(position);
                 info.m_lodLocations[info.m_lodCount] = new Vector4(position.x, position.y, position.z, scale);
                 info.m_lodColors[info.m_lodCount] = color.linear;
