e73f290b8307b15cf6280438e6b56df945ebcba3
MacSergey <macsergey@me.com>
2023-02-12 21:18:34 +0300

    Update API

--- IMT.API/Helper.cs
+++ IMT.API/Helper.cs
@@ -1,13 +1,21 @@
 ﻿using ColossalFramework.Plugins;
 
 using System;
+using System.Reflection;
+using UnityEngine;
 
 namespace IMT.API
 {
     public static class Helper
     {
+        private static ILogHandler Handle { get; } = UnityEngine.Debug.logger.logHandler;
+
+        private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+        private static Version Version => Assembly.GetName().Version;
+
         public static IDataProviderV1 GetProvider(string id)
         {
+            Debug($"Trying to create provider for \"{id}\"");
             foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
             {
                 try
@@ -21,6 +29,7 @@ namespace IMT.API
                             {
                                 var factory = (IDataProviderFactory)Activator.CreateInstance(type);
                                 var provider = factory.GetProvider(id);
+                                Debug($"Provider for \"{id}\" was found: {provider.ModVersion} [{(provider.IsBeta ? "Beta" : "Stable")}]");
                                 return provider;
                             }
                         }
@@ -29,7 +38,10 @@ namespace IMT.API
                 catch { }
             }
 
+            Debug($"Provider for \"{id}\" was not found");
             return null;
         }
+
+        private static void Debug(string message) => Handle.LogFormat(LogType.Log, null, "[{0}][{1}] {2}", $"IMT.API {Version}", Time.realtimeSinceStartup, message);
     }
 }
